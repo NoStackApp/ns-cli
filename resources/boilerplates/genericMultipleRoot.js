831feda3b59fcf4c8e80afd28e6b700482786c74
Rem Lampa <rem@remlampa.com>
2019-12-09 20:11:17 +0800

    Implemented show/hide behavior to root types :sparkles:

--- resources/boilerplates/genericMultipleRoot.js
+++ resources/boilerplates/genericMultipleRoot.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import { Unit } from '@nostack/no-stack';
 import styled from 'styled-components';
+import { v4 } from 'uuid';
+
 import { flattenData } from '../../../flattenData';
 
 import __SingularName__CreationForm from '../__SingularName__CreationForm';
@@ -16,48 +18,81 @@ const __PluralName__StyleWrapper = styled.div`
   justify-content: space-evenly;
 `;
 
-function __PluralName__({ __SingularParentName__Id }) {
-  const parameters = {
-    __CONSTRAINT_VALUE__: __SingularParentName__Id,
+class __PluralName__ extends Component {
+  state = {
+    selected__SingularName__Id: null,
   };
 
-  return (
-    <Unit
-      id={__SOURCE_ID_CONSTANT__}
-      typeRelationships={__RELATIONSHIPS_NAME__}
-      query={__SOURCE_QUERY_NAME__}
-      parameters={parameters}
-    >
-      {({loading, error, data, refetchQueries}) => {
-        if (loading) return 'Loading...';
+  wrapperRef = createRef();
 
-        if (error) {
-          console.error(error);
-          return `Error: ${error.graphQLErrors}`
-        };
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
+  }
 
-        const __PluralNameLowercase__ = data.unitData.map(el => flattenData(el));
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick);
+  }
 
-        return (
-          <>
-          <__PluralName__StyleWrapper>
-            {
-              __PluralNameLowercase__ && __PluralNameLowercase__.map(__SingularNameLowercase__ => (
-                <__SingularName__
-                  key={__SingularNameLowercase__.id}
-                  parentId={__SingularParentName__Id}
-                  __SingularNameLowercase__={__SingularNameLowercase__}
-                  refetchQueries={refetchQueries}
-                />
-              ))
-            }
-            <__SingularName__CreationForm  __SingularParentName__Id={__SingularParentName__Id} refetchQueries={refetchQueries}/>
-          </__PluralName__StyleWrapper>
-          </>
-        );
-      }}
-    </Unit>
-  );
+  handleClick = e => {
+    const node = this.wrapperRef.current;
+
+    if (
+      node &&
+      node !== e.target && 
+      !node.contains(e.target)
+    ) {
+      this.setState({ selected__SingularName__Id: null });
+    }
+  }
+
+  handleSelect = id => this.setState({ selected__SingularName__Id: id });
+
+  render() {
+    const { __SingularParentName__Id } = this.props;
+    const { selected__SingularName__Id } = this.state;
+
+    const parameters = {
+      __CONSTRAINT_VALUE__: __SingularParentName__Id,
+    };
+
+    return (
+      <Unit
+        id={__SOURCE_ID_CONSTANT__}
+        typeRelationships={__RELATIONSHIPS_NAME__}
+        query={__SOURCE_QUERY_NAME__}
+        parameters={parameters}
+      >
+        {({loading, error, data, refetchQueries}) => {
+          if (loading) return 'Loading...';
+
+          if (error) {
+            console.error(error);
+            return `Error: ${error.graphQLErrors}`
+          };
+
+          const __PluralNameLowercase__ = data.unitData.map(el => flattenData(el));
+
+          return (
+            <>
+              <__SingularName__CreationForm  __SingularParentName__Id={__SingularParentName__Id} refetchQueries={refetchQueries}/>
+              <__PluralName__StyleWrapper ref={this.wrapperRef} onClick={this.handleClick}>
+                {__PluralNameLowercase__ && __PluralNameLowercase__.map(__SingularNameLowercase__ => (
+                  <__SingularName__
+                    key={v4()}
+                    parentId={__SingularParentName__Id}
+                    __SingularNameLowercase__={__SingularNameLowercase__}
+                    selected={__SingularNameLowercase__.id === selected__SingularName__Id}
+                    refetchQueries={refetchQueries}
+                    onSelect={this.handleSelect}
+                  />
+                ))}
+              </__PluralName__StyleWrapper>
+            </>
+          );
+        }}
+      </Unit>
+    );
+  }
 }
-export default __PluralName__;
 
+export default __PluralName__;
