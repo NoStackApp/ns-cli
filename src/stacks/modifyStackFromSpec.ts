bfe8f795d58a8e824f9d808774776922563fcc97
YizYah <yyakovson613@gmail.com>
2019-12-09 14:59:01 +0200

    roots showing and hiding

---
+++ src/stacks/modifyStackFromSpec.ts
@@ -0,0 +1,25 @@
+import {UserInfo} from '../constants/types'
+import {genericApiCall} from '../tools/genericApiCall'
+
+const fs = require('fs-extra')
+
+export async function modifyStackFromSpec(templateFile: string, userInfo: UserInfo, sampleEmail: string, addedSuffix: string) {
+  // console.log(`in modifyStackFromSpec, userInfo:${JSON.stringify(userInfo)}`)
+  let templateString = ''
+  try {
+    templateString = await fs.readFile(templateFile, 'utf8')
+
+  } catch (err) {
+    console.error(err)
+    throw new Error(`error building stack from template: ${err}`)
+  }
+
+  const query = `mutation {
+      DataSpec(template: "${templateString.replace(/\n/g, '\\n')}", platformId:"${userInfo.stackId}", sampleEmail: "${sampleEmail}", addedSuffix: "${addedSuffix}")
+  }
+  `
+
+  const returnedData = await genericApiCall(query, userInfo)
+  console.log(`returnedData=${JSON.stringify(returnedData)}`)
+  return returnedData.DataSpec
+}
