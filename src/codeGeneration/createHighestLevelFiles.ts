07d358cd569a397616a3b0eaf19d5845880d42a0
Rem Lampa <rem@remlampa.com>
2020-01-07 20:26:21 +0800

    Implement registration & login boilerplates :sparkles:

--- src/codeGeneration/createHighestLevelFiles.ts
+++ src/codeGeneration/createHighestLevelFiles.ts
@@ -1,39 +1,84 @@
-import {boilerplateDir} from '../constants'
-import {StackInfo} from '../constants/types'
+import { boilerplateDir } from "../constants";
+import { StackInfo } from "../constants/types";
 
-import {compDir, srcDir} from './createTopProjectDirs'
-import {appNameFromPath} from './generateAppCode'
-import {generateAppFile} from './generateAppFile'
+import { compDir, srcDir } from "./createTopProjectDirs";
+import { appNameFromPath } from "./generateAppCode";
+import { generateAppFile } from "./generateAppFile";
 
-const fs = require('fs-extra')
-const Handlebars = require('handlebars')
+const fs = require("fs-extra");
+const Handlebars = require("handlebars");
 
-export async function createHighestLevelFiles(currentStack: StackInfo, appDir: string, userClass: string) {
+export async function createHighestLevelFiles(
+  currentStack: StackInfo,
+  appDir: string,
+  userClass: string
+) {
   // DeleteInstanceMenu
-  await fs.copy(`${boilerplateDir}/DeleteInstanceMenu.js`, `${compDir}/DeleteInstanceMenu/index.js`)
+  await fs.copy(
+    `${boilerplateDir}/DeleteInstanceMenu.js`,
+    `${compDir}/DeleteInstanceMenu/index.js`
+  );
 
   // EditInstanceForm
-  await fs.copy(`${boilerplateDir}/EditInstanceForm.js`, `${compDir}/EditInstanceForm/index.js`)
+  await fs.copy(
+    `${boilerplateDir}/EditInstanceForm.js`,
+    `${compDir}/EditInstanceForm/index.js`
+  );
+
+  // LoginForm
+  await fs.copy(
+    `${boilerplateDir}/LoginForm.js`,
+    `${compDir}/LoginForm/index.js`
+  );
+
+  // RegistrationForm
+  await fs.copy(
+    `${boilerplateDir}/RegistrationForm.js`,
+    `${compDir}/RegistrationForm/index.js`
+  );
+
+  // RegistrationForm.style
+  await fs.copy(
+    `${boilerplateDir}/RegistrationForm.style.js`,
+    `${compDir}/RegistrationForm/RegistrationForm.style.js`
+  );
+
+  // RegistrationField
+  await fs.copy(
+    `${boilerplateDir}/RegistrationField.js`,
+    `${compDir}/RegistrationForm/RegistrationField.js`
+  );
 
   // AuthTabs
-  await fs.copy(`${boilerplateDir}/AuthTabs.js`, `${compDir}/AuthTabs/index.js`)
+  await fs.copy(
+    `${boilerplateDir}/AuthTabs.js`,
+    `${compDir}/AuthTabs/index.js`
+  );
 
   // client file
-  await fs.copy(`${boilerplateDir}/client.js`, `${srcDir}/client/index.js`)
+  await fs.copy(`${boilerplateDir}/client.js`, `${srcDir}/client/index.js`);
 
   // flattenData file
-  await fs.copy(`${boilerplateDir}/flattenData.js`, `${srcDir}/flattenData/index.js`)
+  await fs.copy(
+    `${boilerplateDir}/flattenData.js`,
+    `${srcDir}/flattenData/index.js`
+  );
 
   // index.js
-  await fs.copy(`${boilerplateDir}/index.js`, `${srcDir}/index.js`)
+  await fs.copy(`${boilerplateDir}/index.js`, `${srcDir}/index.js`);
 
   // NavBar
-  const navBar = Handlebars.compile(await fs.readFile(`${boilerplateDir}/NavBar.js`, 'utf-8'))
-  await fs.outputFile(`${compDir}/NavBar/index.js`, navBar({appName: appNameFromPath(appDir)}))
+  const navBar = Handlebars.compile(
+    await fs.readFile(`${boilerplateDir}/NavBar.js`, "utf-8")
+  );
+  await fs.outputFile(
+    `${compDir}/NavBar/index.js`,
+    navBar({ appName: appNameFromPath(appDir) })
+  );
 
   // App file
-  await generateAppFile(currentStack, userClass)
+  await generateAppFile(currentStack, userClass);
 
   // store stack meta data
-  await fs.outputJson(`${appDir}/docs/stack.json`, currentStack)
+  await fs.outputJson(`${appDir}/docs/stack.json`, currentStack);
 }
