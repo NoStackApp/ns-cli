b4a3caea2efe55e570fa2bbd6037e89fbc7098f5
YizYah <yyakovson613@gmail.com>
2020-01-09 17:45:16 +0200

    updated generic api error reporting

--- resources/boilerplates/genericSingleSelectable.js
+++ resources/boilerplates/genericSingleSelectable.js
@@ -5,6 +5,9 @@ import {graphql} from '@apollo/react-hoc';
 
 import { DELETE_{{SingularForRelationshipAllCaps}}_ACTION_ID{{ChildrenTypeList}} } from '../../../config';
 
+// if it has action, import the component
+// import { {{ ActionName }} } from '../{{ ActionName }}';
+
 {{{CHILDREN_IMPORT_LIST}}}
 
 // add styling here
@@ -12,7 +15,7 @@ const {{SingularName}}StyleWrapper = styled.div`
   margin: 2em 1em;
   padding: 1.5em;
   border: none;
-  border-radius: 10px;
+  border-radius: 10px;.3
   box-shadow: 5px 5px 10px #888888;
 `;
 
@@ -49,13 +52,13 @@ function {{SingularName}}({ {{SingularNameLowercase}}, parentId, updateInstance,
       await deleteInstance({
         variables: {
           actionId: DELETE_{{SingularForRelationshipAllCaps}}_ACTION_ID,
-          executionParameters: JSON.stringify({
-            parentInstanceId: parentId,
-            childInstanceId: {{SingularNameLowercase}}.id,
-          }),
-        },
-        refetchQueries
-      });
+        executionParameters: JSON.stringify({
+        parentInstanceId: parentId,
+        childInstanceId: {{SingularNameLowercase}}.id,
+    }),
+    },
+      refetchQueries
+    });
     } catch (e) {
       updateIsDeleting(false);
     }
@@ -63,40 +66,42 @@ function {{SingularName}}({ {{SingularNameLowercase}}, parentId, updateInstance,
 
   return (
     <{{SingularName}}StyleWrapper isDeleting={isDeleting}>
-      { {{SingularNameLowercase}}.value }
-      {isDeleteMode ? (
-          <DeleteMenu>
-            Delete?
-            <Button
-              type="button"
-              hoverColor="#00FF00"
-              onClick={handleDelete}
-              disabled={isDeleting}
-            >
-              &#10003;
-            </Button>
-            <Button
-              type="button"
-              hoverColor="#FF0000"
-              onClick={() => updateIsDeleteMode(false)}
-              disabled={isDeleting}
-            >
-              &#10005;
-            </Button>
-          </DeleteMenu>
-        ) :
-        (
-          <Button
-            type="button"
-            onClick={() => updateIsDeleteMode(true)}
-          >
-            &#128465;
-          </Button>
-        )
-      }
-      {{{CHILDREN_BODY_LIST}}}
-    </{{SingularName}}StyleWrapper>
-  );
+{ {{SingularNameLowercase}}.value }
+{/* if it has contact action, insert button here
+      <{{ActionName}} parentId={ {{SingularNameLowercase}}.id } /> */}
+{isDeleteMode ? (
+    <DeleteMenu>
+      Delete?
+      <Button
+        type="button"
+        hoverColor="#00FF00"
+        onClick={handleDelete}
+        disabled={isDeleting}
+      >
+        &#10003;
+      </Button>
+      <Button
+        type="button"
+        hoverColor="#FF0000"
+        onClick={() => updateIsDeleteMode(false)}
+        disabled={isDeleting}
+      >
+        &#10005;
+      </Button>
+    </DeleteMenu>
+  ) :
+  (
+    <Button
+      type="button"
+      onClick={() => updateIsDeleteMode(true)}
+    >
+      &#128465;
+    </Button>
+  )
+}
+{{{CHILDREN_BODY_LIST}}}
+</{{SingularName}}StyleWrapper>
+);
 }
 
 export default graphql(EXECUTE_ACTION, { name: 'deleteInstance' })({{SingularName}});
