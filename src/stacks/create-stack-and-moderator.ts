2e52fa0471b7a980257e41fcf0ed0cf8ba7d92af
YizYah <yyakovson613@gmail.com>
2020-01-15 16:15:58 +0200

    pause creation of stack to let user confirm moderator

--- src/stacks/create-stack-and-moderator.ts
+++ src/stacks/create-stack-and-moderator.ts
@@ -1,9 +1,27 @@
 const Listr = require('listr')
-
+// const prompts = require('prompts');
+const input = require('listr-input')
 import {loginUser} from '../auth/loginUser'
 import {UserInfo} from '../constants/types'
 
 import {createStackQuery} from './create-stack-query'
+
+// const verifyUser = async () => {
+//   const response = await prompts({
+//     type: 'text',
+//     name: 'userConfirmed',
+//     message: `You must log in your moderator to proceed. Please check your email for a verification
+//             message from 'info@nostack.net' with the title 'Your verification link. When you've verified,
+//             hit enter. `,
+//   })
+//   console.log(response)
+//   // if (response !== 'y') {
+//   //   throw new Error('Please check the email account that you used.  Sometimes emails' +
+//   //     'can take a while to come as well.  You cannot proceed without a confirmed moderator.')
+//   // }
+//
+//   return response
+// }
 
 export async function createStackAndModerator(userInfo: UserInfo) {
   const tasks = new Listr([
@@ -13,12 +31,23 @@ export async function createStackAndModerator(userInfo: UserInfo) {
         await createStackQuery(userInfo)
       }
     },
-    // {
-    //   title: 'Log in the moderator',
-    //   task: async () => {
-    //     await loginUser(userInfo)
-    //   }
-    // },
+    {
+      title: 'Verify moderator identity',
+      task: () => input(`Verify your moderator via email, then hit enter.`, {
+        // validate: value => value.length > 0,
+        // done: credentials => got('https://myapi.com', {
+        //   headers: {
+        //     Authorization: `Bearer ${credentials}`
+        //   }
+        // })
+      })
+    },
+    {
+      title: 'Log in the moderator',
+      task: async () => {
+        await loginUser(userInfo)
+      }
+    },
   ])
 
   return tasks
